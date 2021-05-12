a1c6e9affa77c92f987cef88dd7c1074c1d82e21
MuskanLab <78635537+MuskanLab@users.noreply.github.com>
2021-05-12 09:38:25 +0530

    first comit

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="fix_header_admin_panel"><h3>Admin Panel</h3>
+    </div> 
+    <div class="fix_footer"><h3>@Contain 2020</h3>
+    </div> 
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<!-- -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+<body>
+    
+    <div class="fix_header"><h3>Home</h3>
+    </div> 
+    <div class="section">
+    <a href="/admin">
+    <div class="container1">  
+        <div class="img">
+            <div class="admin_img">
+        <img src="/image/administrator-developer.svg"/></div>
+        </div>
+    </div> 
+    </a>
+    <a href="/user">
+    <div class="container2">     
+        <div class="img">
+            <div class="user_img">
+        <img src="/image/avatar.svg"/></div>
+    </div>
+    </div>
+    </div >
+    </a>
+    <div class="fix_footer"><h3>@Contain 2020</h3>
+    </div> 
+   
+</body>
+</html>
